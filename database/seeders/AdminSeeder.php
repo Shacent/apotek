55ec9d84db495f5e94421df9a43a991d61b2df6f
Shacent <Hauzandini5@gmail.com>
2026-06-04 16:56:12 +0700

    feat: add database acc seed

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,13 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     public function run(): void
     {
         User::create([
